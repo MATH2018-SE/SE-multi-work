c49f0ad20320dabdb45e065db6098cfc5cdbd97f
2959137527 <81426338+2959137527@users.noreply.github.com>
2021-04-26 22:12:08 +0800

    Add files via upload

---
+++ newscollect/Museue Spider/Spider/shoudbwg.py
@@ -0,0 +1,46 @@
+Python 3.6.6 (v3.6.6:4cf1f54eb7, Jun 27 2018, 03:37:03) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> import scrapy
+from MuseumNews.items import MuseumnewsItem
+from scrapy import Spider, Request
+
+URL = "http://www.capitalmuseum.org.cn/zjsb/sbkx_{page}.htm"
+prefixURL = "http://www.capitalmuseum.org.cn/zjsb/"
+
+
+class CapitalNews(scrapy.Spider):
+    name = "CapitalNews"
+    allowed_domains = ['www.capitalmuseum.org.cn']
+    page = 2
+    start_urls = [URL.format(page=page)]
+
+    def parse(self, response):
+        news_body = response.xpath("//td[@height='450']")[0]
+        news_list = news_body.xpath(".//table[@width='85%']")
+        for news in news_list:
+            info = news.xpath(".//text()")
+            if len(info) == 0:
+                continue
+            title = info[1].extract()
+            time = info[0].extract().replace("\xa0", "")
+            content = title
+            href = prefixURL + news.xpath(".//@href")[0].extract()
+            author = "首都博物馆"
+            description = "1"
+            tag = 1
+            item = MuseumnewsItem()
+            item['title'] = title
+            item['author'] = author
+            item['time'] = time
+            item['description'] = description
+            item['content'] = content
+            item['url'] = href
+            item['tag'] = tag
+            yield item
+
+        print('page = {}'.format(self.page))
+        if self.page < 71:
+            self.page += 1
+            new_url = URL.format(page=self.page)
+            print(new_url)
+            yield Request(new_url, callback=self.parse, dont_filter=True)
